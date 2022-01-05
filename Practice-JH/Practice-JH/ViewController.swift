679042f37af537c89378e72470f9dd553cc35c40
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-06 04:38:04 +0900

    [Feat] #7 - 카카오톡 로그인, 계정 로그인 구현
    카카오톡 로그인, 계정 로그인 구현

--- Practice-JH/Practice-JH/ViewController.swift
+++ Practice-JH/Practice-JH/ViewController.swift
@@ -6,14 +6,77 @@
 //
 
 import UIKit
+import KakaoSDKUser
+import KakaoSDKAuth
+import KakaoSDKCommon
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var talkLoginButton: UIButton!
+    @IBOutlet weak var accountLoginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
+    @IBAction func touchTalkLogin(_ sender: Any) {
+        loginKakao()
+    }
+    
+    @IBAction func touchAccountLogin(_ sender: Any) {
+        loginKakaoAccount()
+    }
+    
+    
+}
 
+extension ViewController {
+    func loginKakao() {
+        print("loginKakao() called.")
+        
+        // ✅ 카카오톡 설치 여부 확인
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
+                    
+                    // ✅ 회원가입 성공 시 oauthToken 저장가능하다
+                    // _ = oauthToken
+                    
+                    // ✅ 사용자정보를 성공적으로 가져오면 화면전환 한다.
+                }
+            }
+        }
+        // ✅ 카카오톡 미설치
+        else {
+            print("카카오톡 미설치")
+        }
+    }
+    
+    func loginKakaoAccount() {
+        print("loginKakaoAccount() called.")
+
+        // ✅ 기본 웹 브라우저를 사용하여 로그인 진행.
+        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("loginWithKakaoAccount() success.")
+
+                // ✅ 회원가입 성공 시 oauthToken 저장
+                // _ = oauthToken
+
+                // ✅ 사용자정보를 성공적으로 가져오면 화면전환 한다.
+            }
+        }
+    }
 }
 
