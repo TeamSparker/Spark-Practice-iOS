645d4ce46678ed97b32820cacb7c3d01dde4f4c7
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-07 05:29:27 +0900

    [Feat] #13 - Carousel 스보 및 VC 생성, 화면전환 버튼 구현
    Carousel 스보 및 VC 생성, 화면전환 버튼 구현

---
+++ Practice-JH/Practice-JH/carousel/CarouselVC.swift
@@ -0,0 +1,29 @@
+//
+//  CarouselVC.swift
+//  Practice-JH
+//
+//  Created by Junho Lee on 2022/01/07.
+//
+
+import UIKit
+
+class CarouselVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
