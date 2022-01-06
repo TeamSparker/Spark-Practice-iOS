88f81678a405ca0a70a175a43f6de87e8621a292
yangsubinn <cindy010670@gmail.com>
2022-01-07 02:00:17 +0900

    [Chore] #14 - Stopwatch 프로젝트 세팅

---
+++ Practice-YSB/Stopwatch/Stopwatch/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by 양수빈 on 2022/01/07.
+//
+
+import UIKit
+
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
