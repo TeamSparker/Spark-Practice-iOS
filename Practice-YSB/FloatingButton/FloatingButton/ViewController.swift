b8f3aaf72e4a24448bc997c78c021a77cd6afc23
yangsubinn <cindy010670@gmail.com>
2022-01-07 13:30:11 +0900

    [Chore] #11 - FloatingButton 프로젝트 세팅

---
+++ Practice-YSB/FloatingButton/FloatingButton/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FloatingButton
+//
+//  Created by 양수빈 on 2022/01/07.
+//
+
+import UIKit
+
+import JJFloatingActionButton
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
