47ce1b4c028bc24d3aa51b4a64238e9ce1830da5
yangsubinn <cindy010670@gmail.com>
2022-01-07 14:19:12 +0900

    [Feat] #11 - BaseViewController 적용

--- Practice-YSB/FloatingButton/FloatingButton/ViewController.swift
+++ Practice-YSB/FloatingButton/FloatingButton/ViewController.swift
@@ -10,13 +10,39 @@ import UIKit
 import JJFloatingActionButton
 import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: BaseViewController {
+    
+    // MARK: - Properties
+    /// 뷰에 올릴 프로퍼티 선언
+    let textLabel = UILabel()
 
+    // MARK: - View Life Cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        /// BaseViewController를 상속받기 때문에, BaseViewController에서 viewDidLoad()에 넣어둔 함수는 다시 안넣어도 적용O
     }
 
-
+    // MARK: - Method
+    
+    override func setUI() {
+        super.setUI()
+        
+        /// 프로퍼티 특성 설정
+        textLabel.text = "Floating Button Test"
+        textLabel.textColor = .white
+    }
+    
+    override func setLayout() {
+        /// 프로퍼티를 View에 넣어줌 (storyboard에서 viewcontroller에 올리는 과정)
+        view.addSubview(textLabel)
+        
+        /// textLabel의 레이아웃 설정
+        textLabel.snp.makeConstraints { make in
+            /// superView를 기준으로 center에 배치
+            make.center.equalToSuperview()
+        }
+    }
 }
 
