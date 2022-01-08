8964025e7f6fa8bf7bad6a20fc40a3438dd2c702
yangsubinn <cindy010670@gmail.com>
2022-01-08 17:01:48 +0900

    [Add] #19 - Photo 프로젝트 세팅

---
+++ Practice-YSB/Photo/Photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo
+//
+//  Created by 양수빈 on 2022/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
