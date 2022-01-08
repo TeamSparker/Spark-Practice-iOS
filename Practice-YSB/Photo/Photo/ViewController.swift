0fd6030bcfebc7c80fd0d11c568370b254dc5b7e
yangsubinn <cindy010670@gmail.com>
2022-01-08 17:31:56 +0900

    [Feat] #19 - 앨범에서 사진 가져오기

--- Practice-YSB/Photo/Photo/ViewController.swift
+++ Practice-YSB/Photo/Photo/ViewController.swift
@@ -23,11 +23,11 @@ class ViewController: UIViewController {
         let alter = UIAlertController(title: "사진가져오기", message: "진짜로 사진을 가져오시겠어요?", preferredStyle: .actionSheet)
         
         let library = UIAlertAction(title: "앨범에서 가져오기", style: .default) { action in
-            print("앨범 선택")
+            self.openLibrary()
         }
         
         let camera = UIAlertAction(title: "카메라 열기", style: .default) { action in
-            print("카메라 선택")
+            self.openCamera()
         }
         
         let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
@@ -39,9 +39,28 @@ class ViewController: UIViewController {
         present(alter, animated: true, completion: nil)
     }
     
+    func openLibrary() {
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+            present(picker, animated: false, completion: nil)
+        } else {
+            print("카메라 안됩니다.")
+        }
+    }
 }
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFill
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
 
