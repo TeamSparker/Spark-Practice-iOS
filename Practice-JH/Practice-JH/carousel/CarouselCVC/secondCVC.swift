9025e7820899dce95c313eb7a3e36cbc3bc6f41d
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-13 23:02:29 +0900

    [Feat] #24 - CVCs 커스텀뷰 생성 및 레이아웃
    CVCs 커스텀뷰 생성 및 레이아웃

--- Practice-JH/Practice-JH/carousel/CarouselCVC/secondCVC.swift
+++ Practice-JH/Practice-JH/carousel/CarouselCVC/secondCVC.swift
@@ -8,5 +8,27 @@
 import UIKit
 
 class secondCVC: UICollectionViewCell {
+    let customView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 2
+        view.clipsToBounds = true
+        
+        return view
+    }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.addSubview(self.customView)
+        
+        self.customView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        self.customView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        self.customView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1).isActive = true
+        self.customView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
